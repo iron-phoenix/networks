53d3e9e948f9272e9cf901de9122dd1419859afc
Kirill Luchikhin <kluchikhin@gmail.com>
2014-12-25 07:53:35 +0300

    Add client

---
+++ client-server/client.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from multiprocessing import Process, Pool
+from contextlib import closing
+import socket
+import sys
+import string
+import random
+import json
+import time
+import matplotlib.pyplot as plt
+
+HOST = "127.0.0.1"
+PORT = 5000
+
+def gen_str():
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(random.randint(1, 1000)))
+
+def client_process():
+    request = {"key": gen_str(), "message": gen_str(), "encryption": random.randint(0, 1)}
+    json_request = json.dumps(request)
+    start = time.clock()
+    with closing(socket.socket()) as sock:
+        sock.send(json_request)
+        while 1:
+            data = sock.recv(4096)
+            if not data:
+                break
+    return time.clock() - time
+
+def launch_client(clients_count):
+    p = Pool()
+    asyncs = []
+    for _ in xrange(clients_count):
+        asyncs.append(p.apply_async(client_process))
+    results = [r.get() for r in asyncs]
+    return reduce(lambda a, b: a + b, results) / len(results)
+
+if __name__ == "__main__":
+    results = [launch_client(client_counts) for client_counts in range(5, 55, 5)]
+    plt.plot(range(5, 55, 5), results, "ro-")
+    plt.show()
