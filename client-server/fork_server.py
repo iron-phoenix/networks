3ca861029c6cc47067d30a351f705afa881a0015
Andronik Ordian <andronik.ordian@gmail.com>
2014-12-29 12:10:56 +0300

    fix fork server

--- client-server/fork_server.py
+++ client-server/fork_server.py
@@ -8,12 +8,12 @@ from encryption import TripleDESCipher
 from config import HOST, PORT
 import protocol
 
-def client_process(sock, server_sock, state):
+def client_process(sock, server_sock, timeout=15):
     server_sock.close()
+    sock.settimeout(timeout)
     while 1:
         data = protocol.recv(sock)
         if not data:
-            state.value = False
             return sock.close()
 
         encryption = TripleDESCipher(data["key"])
@@ -23,7 +23,7 @@ def client_process(sock, server_sock, state):
             protocol.send(sock, encryption.decrypt(data["message"]))
 
 
-def server(backlog=5, timeout=15):
+def server(backlog=5):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((HOST, PORT))
@@ -32,7 +32,8 @@ def server(backlog=5, timeout=15):
 
     try:
         while 1:
-            conn, _ = server_socket.accept()
+            conn, addr = server_socket.accept()
+            print "new client " + str(addr)
             proc = Process(target=client_process, args=(conn, server_socket))
             processes.append(proc)
             proc.start()
